950bbaa618468abae23f6c7f45073e264cdaa201
Cholarajarp <190018728+Cholarajarp@users.noreply.github.com>
2026-03-15 04:41:51 +0000

    Fix UI issues in dark and light mode for Streamlit chat input

--- update_readme.py
+++
@@ -1,8 +0,0 @@
-with open("README.md", "r") as f:
-    content = f.read()
-
-# Make sure we don't duplicate
-if "## UI Previews" not in content:
-    print("UI previews not found")
-else:
-    print("README has images, no need to update since images are overwritten in place.")
